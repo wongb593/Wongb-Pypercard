0100bb5f1d75fafe6486c9f60af904b23e19883b
Wong_Benjamin <wongb593@hsd.k12.or.us>
2023-12-04 15:22:37 -0800

    Working on the UI

--- main.py
+++ main.py
@@ -12,108 +12,111 @@ carousel_app = App(
     name="PyperCard carousel", datastore={"counter": 0}
 )
 
-
-@carousel_app.transition("card1", "click", "Card1A")
+@carousel_app.transition("card1", "click", "go_card1A")
 def reset(app, card):
     return "card1A"
 
-@carousel_app.transition("card1", "click", "Wakeup")
+@carousel_app.transition("card1", "click", "go_card1B")
 def onward(app, card):
     return "card1B"
 
-@carousel_app.transition("card1A", "click", "You are late, dress up and go to school")
+@carousel_app.transition("card1A", "click", "go_card0C")
 def forward(app, card):
     return "card0C"
 
-@carousel_app.transition("card1A", "click", "Another 5 minutes")
+@carousel_app.transition("card1A", "click", "go_card2A")
 def go_back(app, card):
     return "card2A"
 
-@carousel_app.transition("card2A", "click", "your mom finds out you are still sleeping and beats you up")
+@carousel_app.transition("card2A", "click", "go_card1")
 def reset(app, card):
     return "card1"
 
-@carousel_app.transition("card1B", "click", "Go to school early for no reason")
+@carousel_app.transition("card1B", "click", "go_card0C")
 def reset(app, card):
     return "card0C"
 
-@carousel_app.transition("card1B", "click", "Go eat breakfast")
+@carousel_app.transition("card1B", "click", "go_card2B")
 def reset(app, card):
     return "card2B"
 
-@carousel_app.transition("card2B", "click", "Go to school")
+@carousel_app.transition("card2B", "click", "go_card1C")
 def reset(app, card):
     return "card1C"
 
-@carousel_app.transition("card2B", "click", "Skip school for the convenience store")
+@carousel_app.transition("card2B", "click", "go_card3B")
 def reset(app, card):
     return "card3B"
 
-@carousel_app.transition("card3B", "click", "As you are buying your items you mom finds you and beats you")
+@carousel_app.transition("card3B", "click", "go_card1")
 def reset(app, card):
     return "card1"
 
-@carousel_app.transition("card0C", "click", "reset")
+@carousel_app.transition("card0C", "click", "go_card1")
 def reset(app, card):
     return "card1"
 
-@carousel_app.transition("card1C", "click", "Go to physical education")
+@carousel_app.transition("card1C", "click", "go_card1D")
 def reset(app, card):
     return "card1D"
 
-@carousel_app.transition("card1C", "click", "Go to a random class")
+@carousel_app.transition("card1C", "click", "go_card2C")
 def reset(app, card):
     return "card2C"
 
-@carousel_app.transition("card2C", "click", "Back to class")
+@carousel_app.transition("card2C", "click", "go_card2DB")
 def reset(app, card):
     return "card2DB"
 
-@carousel_app.transition("card1D", "click", "Go eat lunch")
+@carousel_app.transition("card1D", "click", "go_card2D")
 def reset(app, card):
     return "card2D"
 
-@carousel_app.transition("card1D", "click", "Go and talk to our friends and completely neglect your stomach")
+@carousel_app.transition("card1D", "click", "go_card3D")
 def reset(app, card):
     return "card3D"
 
-@carousel_app.transition("card2D", "click", "Go steal lunch money from a small kid")
+@carousel_app.transition("card2D", "click", "go_card2DA")
 def reset(app, card):
     return "card2DA"
 
-@carousel_app.transition("card2D", "click", "Ask a friend who might think lowly of you since you asked them for	money")
+@carousel_app.transition("card2D", "click", "go_card4D")
 def reset(app, card):
     return "card4D"
 
-@carousel_app.transition("card2D", "click", "Do not eat and wait lunch out")
+@carousel_app.transition("card2D", "click", "go_card3D")
 def reset(app, card):
     return "card3D"
 
-@carousel_app.transition("card2DA", "click", "Pass till after detention")
+@carousel_app.transition("card2DA", "click", "go_card2DB")
 def reset(app, card):
     return "card2DB"
 
-@carousel_app.transition("card2DB", "click", "reset")
+@carousel_app.transition("card2DB", "click", "go_card1")
+def reset(app, card):
+    return "card1"
+
+@carousel_app.transition("card3D", "click", "go_card1")
 def reset(app, card):
     return "card1"
     
-@carousel_app.transition("card4D", "click", "Next")
+@carousel_app.transition("card4D", "click", "go_card1E")
 def reset(app, card):
     return "card1E"
 
-@carousel_app.transition("card1E", "click", "Go wander the halls for no reason")
+@carousel_app.transition("card1E", "click", "go_card2E")
 def reset(app, card):
     return "card2E"
 
-@carousel_app.transition("card3D", "click", "Go to next class")
+@carousel_app.transition("card1E", "click", "go_card1F")
 def reset(app, card):
     return "card1F"
 
-@carousel_app.transition("card2E", "click", "reset")
+@carousel_app.transition("card2E", "click", "go_card1")
 def reset(app, card):
     return "card1"
 
-@carousel_app.transition("card1F", "click", "Another day")
+@carousel_app.transition("card1F", "click", "go_card1")
 def reset(app, card):
     return "card1"
 
